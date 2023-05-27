b5886cc09843708c96337ba29ea48bbeb2583aa1
Fadi Zoghlami <zoghlamifadi77@gmail.com>
2023-05-27 20:40:21 +0200

    Updated Data Augmentation

--- utils/data_augmentation_utils.py
+++ utils/data_augmentation_utils.py
@@ -41,94 +41,95 @@ class GaussianBlur(object):
         return sample
 
 
-class simCLR_training_data_augmentation(object):
+class simCLR_training_data_augmentation():
     """
     Implementation of the data augmentations on the trainig data
-    proposed in the SimCLR official paper
+    as proposed in the SimCLR official paper
     """
 
     def __init__(
         self,
-        input_height: int = 224,
+        size: int = 224,
         gaussian_blur: bool = False,
-        jitter_strength: float = 1.,
+        jitter_strength = 1,
         normalize: Optional[transforms.Normalize] = None
-    ) -> None:
+    ):
 
         self.jitter_strength = jitter_strength
-        self.input_height = input_height
+        self.size = size
         self.gaussian_blur = gaussian_blur
         self.normalize = normalize
 
         self.color_jitter = transforms.ColorJitter(
-            0.8 * self.jitter_strength, # type: ignore
-            0.8 * self.jitter_strength, #type: ignore
-            0.8 * self.jitter_strength, #type: ignore
-            0.2 * self.jitter_strength  #type: ignore
+            0.8 * self.jitter_strength,
+            0.8 * self.jitter_strength, 
+            0.8 * self.jitter_strength, 
+            0.2 * self.jitter_strength
         )
 
         data_transforms = [
-            transforms.RandomResizedCrop(size=self.input_height),
+            transforms.RandomResizedCrop(size=self.size),
             transforms.RandomHorizontalFlip(p=0.5),
             transforms.RandomApply([self.color_jitter], p=0.8),
             transforms.RandomGrayscale(p=0.2)
         ]
 
+        # Adding Gaussian blur
         if self.gaussian_blur:
-            data_transforms.append(GaussianBlur(kernel_size=int(0.1 * self.input_height), p=0.5))
+            data_transforms.append(GaussianBlur(kernel_size=int(0.1 * self.size), p=0.5))
+
+        # Adding Normalization
+        if self.normalize:
+            data_transforms.append(self.normalize)
 
         data_transforms.append(transforms.ToTensor())
-
-        if self.normalize:
-            data_transforms.append(normalize)
 
         # Transformations on the training data
         self.train_transform = transforms.Compose(data_transforms)
 
     def __call__(self, z):
-        transform = self.train_transform
-
-        z_i = transform(z)
-        z_j = transform(z)
+        z_i = self.train_transform(z)
+        z_j = self.train_transform(z)
 
         return z_i, z_j
 
 
-class simCLR_eval_data_augmentation(object):
+class simCLR_eval_data_augmentation():
     """
     Implementation of the data augmentations on the testing data
-    proposed in the SimCLR official paper
+    as proposed in the SimCLR official paper
     """
 
     def __init__(
         self,
-        input_height: int = 224,
+        size: int = 224,
         crop: bool = False,
         normalize: Optional[transforms.Normalize] = None
     ):
-        self.input_height = input_height
+        self.size = size
+        self.crop = crop
         self.normalize = normalize
 
         data_transforms = [
-            transforms.Resize(self.input_height),
+            transforms.Resize(self.size),
             transforms.ToTensor()
         ]
 
-        if crop:
-            data_transforms.append(transforms.RandomResizedCrop(size=self.input_height))
+        # Adding Crop
+        if self.crop:
+            data_transforms.append(transforms.RandomResizedCrop(size=self.size))
         
-        data_transforms.append(transforms.ToTensor())
-
+        # Adding Normalization
         if self.normalize:
             data_transforms.append(normalize)
+
+        data_transforms.append(transforms.ToTensor())
 
         # Transformations on the testing data
         self.test_transform = transforms.Compose(data_transforms)
 
     def __call__(self, z):
-        transform = self.test_transform
-
-        z_i = transform(z)
-        z_j = transform(z)
+        z_i = self.test_transform(z)
+        z_j = self.test_transform(z)
 
         return z_i, z_j
