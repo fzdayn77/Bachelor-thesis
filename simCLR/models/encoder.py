85396327c4ae584dccaa9a629a1e0ed512f14f8d
Fadi Zoghlami <zoghlamifadi77@gmail.com>
2023-06-02 14:21:56 +0200

    Added encoder.py

--- simCLR/models/encoder.py
+++ simCLR/models/encoder.py
@@ -1,2 +1,17 @@
-def get_encoder():
-    pass
+from .resnet import get_resnet
+from .forward_forward import Forward_Forward
+
+def get_encoder(name):
+    # All possible encoders
+    encoders = ("resnet18", "resnet34", "resnet50", "forward-forward")
+
+    if name not in encoders:
+        raise KeyError(f"{name} is not a valid encoder name")
+    
+    # ResNet18/34/50
+    if name == "resnet18" or name == "resnet34" or name == "resnet50":
+        return get_resnet(name=name, pretrained=False)
+    
+    # Forward-Forward
+    if name == "forward-forward":
+        return Forward_Forward()
